ff1aa5a9d5a3a5fdeda2ef11cfcf5af9f0d692d4
Lachlan Heywood <lachieh@users.noreply.github.com>
2023-01-24 17:10:27 -0500

    remove testing-lib from cypress mixin

--- mixins/testing-cypress.js
+++ mixins/testing-cypress.js
@@ -5,7 +5,7 @@ module.exports = {
   overrides: [
     {
       files: ['**/cypress/**/*.[jt]s?(x)', '**/*.cypress.[jt]s?(x)'],
-      extends: ['plugin:testing-library/dom', 'plugin:cypress/recommended'],
+      extends: ['plugin:cypress/recommended'],
       parserOptions: {
         project: './cypress/tsconfig.json',
       },
